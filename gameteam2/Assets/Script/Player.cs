57d33a5d8b949039a51f1805cd0fde9943c2cf07
Kim12305 <78015118+Kim12305@users.noreply.github.com>
2021-05-27 14:31:03 +0900

    Kim
    사다리타기

--- gameteam2/Assets/Script/Player.cs
+++ gameteam2/Assets/Script/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Rigidbody2D myrigidbody;
+
     [SerializeField]
     private float moveSpeed = 5f;
     [SerializeField]
@@ -17,6 +19,7 @@ public class Player : MonoBehaviour
     void Start()
     {
         InvokeRepeating("RandomMonster", 10, 3); //10초후 부터, _Random함수를 3초마다 반복해서 실행 시킵니다.
+        myrigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -27,6 +30,22 @@ public class Player : MonoBehaviour
         //만약 단서가 0개면 여긴 단서가 없어...라고 말함
         //숨을때 레이어 바꾸기 프로젝트세팅 -피직스 -레이어 콜리즌 메트릭스들어가면 이 레이어끼리 콜라이더를 할거냐 안할거냐(이것도 강의에 있던 것 같아) 박스 콜라이더 닳으면 HP닳아
         //몬스터 닿으면 이미지 띄우고 HP 닳아야지 그 후 애니메이션 쓰러짐->일어남... 이건 좀 애매한데 못하겠음 기획자한테 말하기
+    }
+
+    private void FixedUpdate()
+    {
+        if(isLadder)
+        {
+            float ver = Input.GetAxis("Vertical");
+            myrigidbody.gravityScale = 0;
+            myrigidbody.velocity = new Vector2(myrigidbody.velocity.x, ver * moveSpeed);
+        }
+
+        else
+        {
+            myrigidbody.gravityScale = 5f;
+        }
+
     }
 
     private void PlayerMove()
@@ -53,6 +72,31 @@ public class Player : MonoBehaviour
         }
     }
 
+    //사다리(?)이동
+    public bool isLadder;
+
+    private void OnTriggerEnter2D(Collider2D collision)//사다리 인식하기
+    {
+        if(collision.CompareTag("Ladder"))//ladder라는 태그를 만나면?
+        {
+            isLadder = true;
+            
+            //사다리가 true면 위아래로 움직이게 한다
+            //Debug.Log("사다리 만남");
+        }
+
+        
+    }
+    private void OnTriggerExit2D(Collider2D collision)//사다리 빠져나오면?
+    {
+        if (collision.CompareTag("Ladder"))//ladder라는 태그를 만나면?
+        {
+            isLadder = false;
+        }
+    }
+     
+
+
     private void RandomMonster()
     {
         if (GameManager.instance.IsMonster == false) //몬스터
@@ -66,4 +110,5 @@ public class Player : MonoBehaviour
             }
         }
     }
+
 }
