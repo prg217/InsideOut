2a540bf922a02fddc3bd3a22b596593bfb10a5d3
Kim12305 <78015118+Kim12305@users.noreply.github.com>
2021-05-31 11:45:37 +0900

    1
    일단은 사다리 모션 걷기 모션 구현
    레이저 관련 범위 수정
    사람 스프라이트 크기 조절
    벽 태그 수정
    사다리 코드 위아래 뚫기 수정

---
+++ ani/gameteam2/Assets/Script/Player.cs
@@ -0,0 +1,174 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D myrigidbody;
+
+    [SerializeField]
+    private float moveSpeed = 5f;
+    [SerializeField]
+    private float random = 0;
+    [SerializeField]
+    private GameObject monsterObj = null;
+    [SerializeField]
+    private GameObject monsterPos = null;
+
+    public bool isLadder;
+    public bool isWalk;
+    public bool doLadder;
+    public static bool move = true;
+
+    public Animator animator;
+    public float ladderSpeed = 0.0f;
+    
+
+
+    //public int HP = 20;
+
+    void Start()
+    {
+        //애니메이션 초기화
+        animator = GetComponent<Animator>();
+
+        isWalk = true;
+       
+
+        InvokeRepeating("RandomMonster", 10, 3); //10초후 부터, _Random함수를 3초마다 반복해서 실행 시킵니다.
+        myrigidbody = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         PlayerMove();
+        
+
+        //만약 단서가 0개면 여긴 단서가 없어...라고 말함
+        //숨을때 레이어 바꾸기 프로젝트세팅 -피직스 -레이어 콜리즌 메트릭스들어가면 이 레이어끼리 콜라이더를 할거냐 안할거냐(이것도 강의에 있던 것 같아) 박스 콜라이더 닳으면 HP닳아
+        //몬스터 닿으면 이미지 띄우고 HP 닳아야지 그 후 애니메이션 쓰러짐->일어남... 이건 좀 애매한데 못하겠음 기획자한테 말하기
+    }
+
+    private void FixedUpdate()
+    {
+        if(isLadder)
+        {
+            float ver = Input.GetAxis("Vertical");
+            myrigidbody.gravityScale = 0;
+            myrigidbody.velocity = new Vector2(myrigidbody.velocity.x, ver * moveSpeed);
+
+            doLadder = true;
+
+            if(doLadder == true)
+            {
+
+                if (ver > 0)
+                {
+                    animator.SetBool("doLadder", true);
+                    animator.SetBool("Ladder_Idle", false);
+                }
+                else if (ver == 0)
+                {
+                       animator.SetBool("doLadder", false);
+                       animator.SetBool("Ladder_Idle", true);
+
+                }
+                else
+                {
+                    animator.SetBool("doLadder", true);
+                    animator.SetBool("Ladder_Idle", false);
+                }
+            }
+
+        }
+
+        else
+        {
+            myrigidbody.gravityScale = 5f;
+          
+        }
+
+    }
+
+    private void PlayerMove()
+    {      
+        float h = Input.GetAxis("Horizontal");
+        float playerSpeed = h * moveSpeed * Time.deltaTime;
+        Vector3 vector3 = new Vector3();
+        vector3.x = playerSpeed;
+        transform.Translate(vector3);
+
+        if (h < 0)
+        { 
+            transform.localScale = new Vector3(-0.23f, 0.23f, 0.23f);
+            animator.SetBool("isWalk", true);
+        }
+        else if (h == 0)
+        {
+           GetComponent<Animator>().SetBool("isWalk", false);
+        }
+        else
+        {            
+            transform.localScale = new Vector3(0.23f, 0.23f, 0.23f);
+            animator.SetBool("isWalk", true);
+        }
+    }
+
+    //사다리(?)이동
+    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Ladder"))//사다리 인식하기 ladder라는 태그를 만나면?
+        {
+            isLadder = true;
+            
+            //사다리가 true면 위아래로 움직이게 한다
+            //Debug.Log("사다리 만남");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)//사다리 빠져나오면?
+    {
+        if (collision.CompareTag("Ladder"))//ladder라는 태그를 만나면?
+        {
+            isLadder = false;
+            
+        }
+    }
+     
+
+    private void RandomMonster()
+    {
+        if (GameManager.instance.IsMonster == false) //몬스터
+        {
+            random = Random.Range(0, 1000);
+
+            if (random > 900)
+            {
+                GameManager.instance.IsMonster = true;
+                GameObject monster = (GameObject)Instantiate(monsterObj, monsterPos.transform.position, Quaternion.identity);
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision) //접촉하고 있을 때
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (Input.GetKey(KeyCode.LeftControl)) //좌 컨트롤 누르고 있을 동안만
+            {
+                GameObject.Find("Player").layer = 6; //Hide
+                Player.move = false;
+            }
+            else //떼었을 때
+            {
+                GameObject.Find("Player").layer = 0; //디폴트(다시 몬스터 맞음)
+                Player.move = true;
+            }
+        }
+    }
+}
+
+
