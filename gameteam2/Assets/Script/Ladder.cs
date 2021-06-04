0cc11fb5564b852c93cd720547ffe948c25d8b16
Kim12305 <78015118+Kim12305@users.noreply.github.com>
2021-06-04 12:41:00 +0900

    방 구조+빛
    방 구조 틀 짜기
    어둠주기
    특정 방 콜라이더로 막기

--- gameteam2/Assets/Script/Ladder.cs
+++ gameteam2/Assets/Script/Ladder.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Ladder : MonoBehaviour
 {
     public Collider2D platformCollider;
+    public Collider2D platformCollider1;
     //사다리 밑으로도 뚫기
 
     private void OnTriggerStay2D(Collider2D collision)
@@ -12,6 +13,7 @@ public class Ladder : MonoBehaviour
         if (collision.CompareTag("Player")) //플레이어일경우 뚫기
         {
             Physics2D.IgnoreCollision(collision.GetComponent<Collider2D>(), platformCollider, true);
+            Physics2D.IgnoreCollision(collision.GetComponent<Collider2D>(), platformCollider1, true);
         }
     }
 
@@ -21,6 +23,7 @@ public class Ladder : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             Physics2D.IgnoreCollision(collision.GetComponent<Collider2D>(), platformCollider, false);
+            Physics2D.IgnoreCollision(collision.GetComponent<Collider2D>(), platformCollider1, false);
         }
     }
 }
--- gameteam2/Library/PackageCache/com.unity.2d.spriteshape@5.1.2/Runtime/SpriteShapeGeometryCache.cs
+++ gameteam2/Library/PackageCache/com.unity.2d.spriteshape@5.1.2/Runtime/SpriteShapeGeometryCache.cs
@@ -51,6 +51,10 @@ internal class SpriteShapeGeometryCache : MonoBehaviour
     NativeArray<ushort> m_IndexArrayCache;
     NativeArray<UnityEngine.U2D.SpriteShapeSegment> m_GeomArrayCache;
 
+    internal ushort[] indexArray
+    {
+        get { return m_IndexArray; }
+    }
     internal int maxArrayCount
     {
         get { return m_MaxArrayCount; }
@@ -100,13 +104,14 @@ internal class SpriteShapeGeometryCache : MonoBehaviour
 
             m_PosArray = new Vector3[vertexCount];
             m_Uv0Array = new Vector2[vertexCount];
-            m_TanArray = new Vector4[m_TanArrayCache.Length];
+            m_TanArray = new Vector4[vertexCount];
             m_IndexArray = new ushort[indexCount];
 
+            SpriteShapeCopyUtility<ushort>.Copy(m_IndexArray, m_IndexArrayCache, indexCount);
             SpriteShapeCopyUtility<Vector3>.Copy(m_PosArray, m_PosArrayCache, vertexCount);
             SpriteShapeCopyUtility<Vector2>.Copy(m_Uv0Array, m_Uv0ArrayCache, vertexCount);
-            SpriteShapeCopyUtility<Vector4>.Copy(m_TanArray, m_TanArrayCache, m_TanArrayCache.Length);
-            SpriteShapeCopyUtility<ushort>.Copy(m_IndexArray, m_IndexArrayCache, indexCount);
+            if (m_TanArrayCache.Length >= vertexCount)
+                SpriteShapeCopyUtility<Vector4>.Copy(m_TanArray, m_TanArrayCache, vertexCount);
 
             m_MaxArrayCount = (vertexCount > indexCount) ? vertexCount : indexCount;
             m_RequiresUpdate = false;
